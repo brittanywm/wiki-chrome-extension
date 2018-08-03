c7f1c7fcf25a52c7791a9ba23e08aa97ce6174e9
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-08-03 11:23:57 -0700

    Added wiki icon, Added message when no search results are found

--- wiki-search-ext/src/browser_action/main.js
+++ wiki-search-ext/src/browser_action/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
+  $(".no-result-found").hide();
   $("#btn").click(function () {
+    //$(".no-result-found").hide();
     var url = "https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&origin=*&srsearch="
     var searchInfo = "";
     searchInfo = $("#mySearch").val();
@@ -14,9 +16,14 @@ $(document).ready(function () {
           linksHTML += "<a href='" + sendUrl + value.title.replace(/ /g, "_") + "'target='_blank'>" + "<br />" +"<span class='titleIncrease'>" +value.title +"</span>"+ "<br />" + "<p>" + value.snippet + "</p>" + "</a>";
           console.log("link title", value.title);
           console.log(value.snippet);
-
         });
         linksHTML += "</div>"
+        console.log("links length", linksHTML.length);
+        if (linksHTML.length < 12) {
+          $(".no-result-found").show();
+        } else {
+          $(".no-result-found").hide()
+        }
         $(".inner").html(linksHTML);
       })
       .fail(function (error) {
